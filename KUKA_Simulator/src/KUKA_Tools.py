770b391aabae8c1f4c6a93113bc6be7f9c5737da
EWa74 <eric.wahl@web.de>
2014-05-27 22:14:30 +0200

    https://github.com/EWa74/KUKA_Simulator.git

--- KUKA_Simulator/src/KUKA_Tools.py
+++ KUKA_Simulator/src/KUKA_Tools.py
@@ -1,5 +1,5 @@
 #  ***** BEGIN GPL LICENSE BLOCK *****
-#  git
+#  https://github.com/EWa74/KUKA_Simulator.git
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
