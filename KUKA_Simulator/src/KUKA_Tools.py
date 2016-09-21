ff4626a980615e1ce676a95c5993b052c3f3f611
ewa <ewa@ewa-dell-laptop.IEE.intern>
2016-09-21 22:24:13 +0200

    2

--- KUKA_Simulator/src/KUKA_Tools.py
+++ KUKA_Simulator/src/KUKA_Tools.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*- 
 
-#!/usr/bin/env python
+#!/usr/bin/env python 
 # -*- coding: utf-8 -*-
 # coding Angabe in Zeilen 1 und 2 fuer Eclipse Luna/ Pydev 3.9 notwendig
  # cp1252
